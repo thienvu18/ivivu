0bf7f8093ee2a67c51d9ed465859e6123912a03a
haiau762@gmail.com <haiau762@gmail.com>
2018-12-11 11:00:57 +0700

    Up...

--- App/ivivuApp/ivivuApp/SignUp.xaml.cs
+++ App/ivivuApp/ivivuApp/SignUp.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
 
 namespace ivivuApp
 {
@@ -26,9 +30,17 @@ namespace ivivuApp
 
         private void btn_confirm_Click(object sender, RoutedEventArgs e)
         {
-        
+            SqlCommand cmd = new SqlCommand("proc_signUpUser", Database.connection);
 
+            cmd.CommandType = CommandType.StoredProcedure;
 
+            cmd.Parameters.Add(new SqlParameter("@FullName", txt_fullname.Text));
+            cmd.Parameters.Add(new SqlParameter("@UserName", txt_username.Text));
+            cmd.Parameters.Add(new SqlParameter("@Pass", txt_pass.Password));
+            cmd.Parameters.Add(new SqlParameter("@CMND", txt_cmnd.Text));
+            cmd.Parameters.Add(new SqlParameter("@SDT", txt_phone.Text));
+            cmd.Parameters.Add(new SqlParameter("@Address", txt_address.Text));
+            cmd.Parameters.Add(new SqlParameter("@Email", txt_email.Text));
             var window = new Login_user();
             this.Close();
             window.ShowDialog();
